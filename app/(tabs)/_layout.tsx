37cc14dd98497952a32af0fd991c40db653b02eb
adelson70 <bittencourtadelson1@gmail.com>
2026-06-14 11:22:49 -0300

    GUI

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import { Tabs } from "expo-router";
+import { ThermometerIcon, GearIcon, MagnifyingGlassIcon } from "phosphor-react-native";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen 
+      name="search" 
+      options={{ 
+        title: "Buscar",
+        tabBarIcon: ({color, size}) => (
+          <MagnifyingGlassIcon size={size} color={color} />
+        )
+       }} 
+      />
+
+      <Tabs.Screen 
+      name="home" 
+      options={{ 
+        title: "Clima",
+        tabBarIcon: ({ color, size}) => (
+          <ThermometerIcon size={size} color={color} />
+        ) 
+      }} 
+      />
+      
+      <Tabs.Screen 
+      name="settings" 
+      options={{ 
+        title: "Config",
+        tabBarIcon: ({ color, size }) => (
+          <GearIcon size={size} color={color} />
+        )
+       }} 
+      />
+    </Tabs>
+  );
+}
